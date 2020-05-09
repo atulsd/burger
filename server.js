d175fdaf1442b8b667ec0416b91aec5e87995966
Atul Mahajan <luta_atul@hotmail.com>
2020-05-09 18:03:51 +1000

    Cleaning file.

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var exphbs = require("express-handlebars");
 var PORT = process.env.PORT || 3000;
 
 var app = express();
+
+// Set Handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 var routes = require("./controllers/burgers_controller.js");
 
 // Serve static content for the app from the "public" directory in the application directory.
@@ -11,10 +16,6 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
-
-// Set Handlebars.
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 app.listen(PORT, function () {
   console.log("App now listening at localhost:" + PORT);
