382d23d830aa93478c6317de4f097f676090df5c
Atul Mahajan <luta_atul@hotmail.com>
2020-05-09 18:04:14 +1000

    Cleaning file.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,7 +9,6 @@ router.get("/", function (req, res) {
     var hbsObject = {
       burgers: data,
     };
-    console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
@@ -17,7 +16,7 @@ router.get("/", function (req, res) {
 router.post("/api/burgers", function (req, res) {
   burger.insertOne(
     ["burger_name", "devoured"],
-    [req.body.name, false],
+    [req.body.name, req.body.devoured === "true"],
     function (result) {
       res.json({ id: result.insertId });
     }
